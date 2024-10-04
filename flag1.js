9c15ae1b2d184f4900b1ce6d167c7af36ff6846f
itshalib <Hackmehalib@gmail.com>
2024-10-04 13:48:53 -0400

    Update flag1.js

--- flag1.js
+++ flag1.js
@@ -1,15 +1,35 @@
-// flag1
-function generateObfuscatedString() {
+// Flag1 
+function encodeString() {
     
-    const c = [
-        81, 87, 69, 84, 82, 89, 
+    const d86cf9d4cc444445b230ad02c7f14cecd86cf9d4cc444445b230ad02c7f14cec
+     = [
+        81, 87, 69, 84, 82, 89,
         49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 
         65, 83, 68, 70, 71, 72, 74, 75, 76, 
-        90, 88, 67, 86, 66, 78, 77, 33, 64, 35 
+        90, 88, 67, 86, 66, 78, 77,
+        33, 64, 35 
     ];
 
-  
-    let result = String.fromCharCode(...c);
+   
+    return d86cf9d4cc444445b230ad02c7f14cecd86cf9d4cc444445b230ad02c7f14cec.map(code => {
+       
+        return (code * 2) + 10;
+    });
+}
+
+
+function decodeCodes(codes) {
+   
+    return String.fromCharCode(...codes.map(code => {
+        
+        return (code - 10) / 2; 
+    }));
+}
+
+
+function generateObfuscatedString() {
+    const encoded = encodeString();
+    const result = decodeCodes(encoded);
     return result;
 }
 
@@ -18,5 +38,6 @@ function displayString() {
     const specificString = generateObfuscatedString();
     document.getElementById("output").innerText = specificString;
 }
+
 
 document.addEventListener("DOMContentLoaded", displayString);
